56649c94feb89044770dc6155a754428f4f07e75
Keven <kevenleonardo11@gmail.com>
2024-04-02 21:34:22 -0300

    finalmente funcionou

---
+++ aula1.js
@@ -0,0 +1,20 @@
+/*
+
+    programa de computador
+    E -> P -> S
+
+    javascript nao é o mesmo que JAVA!!!
+    C, C++, C#, js, PHP -> C like
+    python, ruby, lisp visual basic
+
+    js é fracamente tipado
+    js é interpretado
+    tipagem dinamica
+*/
+
+var nome = "Keven"; // texto -> string
+var idade = 23; // numero -> number -> int
+var peso = 54.9; // number -> float
+var acordado = true; // boolean -> bool
+
+nome - "luluzinha";
